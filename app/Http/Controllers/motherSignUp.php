3437a5c89023665b7a15055c2466de51e61738a2
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2020-12-26 20:00:45 +0600

    mother sign up data stored on database

--- app/Http/Controllers/motherSignUp.php
+++ app/Http/Controllers/motherSignUp.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\mother_info;
 use Illuminate\Http\Request;
 
 class motherSignUp extends Controller
 {
     function create(Request $request){
 
-        return $request->input();
+        //register new user
+        $mother = new mother_info();
+        $mother->name = $request->Mother_name;
+        $mother->birthdate = $request->Mother_birthdate;
+        $mother->occupation = $request->Mother_occupation;
+        $mother->income_per_month = $request->Mother_income;
+        $mother->nid = $request->Mother_nid;
+        $mother->address = $request->Mother_address;
+        $mother->phone = $request->Mother_phone;
+
+        $query = $mother->save();
+
+        if($query){
+            return "Success";
+            //return redirect()->route('signUpFather');
+        }
+        else
+            return  "Something went wrong";
+        //return $request->input();
     }
 }
