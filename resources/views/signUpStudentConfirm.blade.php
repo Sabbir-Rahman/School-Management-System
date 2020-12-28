0620eb9709aee9726fe99903b2144ab7bd39cec1
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2020-12-28 15:38:58 +0600

    student sign up confirmation

---
+++ resources/views/signUpStudentConfirm.blade.php
@@ -0,0 +1,8 @@
+<h2>Student succesfully added to database</h2>
+<h2>Student name {{$name}}</h2>
+<h2>Student id: {{$id}}</h2>
+<h2>Student batch: {{$batch}}</h2>
+<h2>Student class: {{$class}}</h2>
+<h2>Student shift: {{$shift}}</h2>
+<h2>Student section: {{$section}}</h2>
+<button>Continue to father signup</button>
