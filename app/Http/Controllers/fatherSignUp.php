22cf882f9a1195982d96119eb38367b10f472fba
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2020-12-29 12:11:13 +0600

    Father succesfully added to database

--- app/Http/Controllers/fatherSignUp.php
+++ app/Http/Controllers/fatherSignUp.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\father_info;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class fatherSignUp extends Controller
 {
     public $studentId;
     public $fatherId='';
+    public $studentCount;
     function create(Request $request){
 
         $this->studentId = $request->Student_id;
+        $this->studentCount = DB::table('student_infos')
+            ->where('id',$this->studentId)
+            ->count();
+
+        if($this->studentCount==0)
+            return "Student not exist so how can student father??";
+
+
         $this->fatherId = strval($this->studentId).'1';
         //register new user
         $father = new father_info();
@@ -23,11 +33,13 @@ class fatherSignUp extends Controller
         $father->nid = $request->Father_nid;
         $father->address = $request->Father_address;
         $father->phone = $request->Father_phone;
+        $father->password = $request->Father_password;
+        $father->comment = $request->Father_comment;
 
         $query = $father->save();
 
         if($query){
-            return view('signUpMother');
+            return view('signUpFatherConfirm',['id'=>$father->id,'name'=> $father->name]);
             //return redirect()->route('signUpFather');
         }
         else
