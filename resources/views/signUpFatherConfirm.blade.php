bc56acdc2241c33e5fe862322a44de97e834e11a
SaiyaraOishi <56593485+SaiyaraOishi@users.noreply.github.com>
2021-01-03 01:16:38 +0600

    test

--- resources/views/signUpFatherConfirm.blade.php
+++ resources/views/signUpFatherConfirm.blade.php
@@ -11,7 +11,8 @@
     <link rel="stylesheet" href="/css/signUpFatherConfirm-1.css" media="screen">
     <script class="u-script" type="text/javascript" src="/js/signUpFatherConfirm.js" defer=""></script>
     <meta name="generator" content="Nicepage 3.2.3, nicepage.com">
-    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-theme-google-font" rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
 
 
     <script type="application/ld+json">{
@@ -19,7 +20,8 @@
 		"@type": "Organization",
 		"name": "",
 		"url": "index.html"
-}</script>
+}
+    </script>
     <meta property="og:title" content="Page 1">
     <meta property="og:type" content="website">
     <meta name="theme-color" content="#478ac9">
@@ -29,15 +31,15 @@
 <body data-home-page="Page-1.html" data-home-page-title="Page 1" class="u-body">
 <section class="u-clearfix u-image u-section-1" id="sec-e271" data-image-width="1280" data-image-height="960">
     <div class="u-clearfix u-sheet u-sheet-1">
-        <div class="u-container-style u-group u-opacity u-opacity-85 u-palette-1-base u-radius-50 u-shape-round u-group-1">
-            <div class="u-container-layout u-container-layout-1">
-                <h6>Father succesfully added to database.</h6>
-                <h6>Father name: {{$name}}</h6>
-                <h6><font color="blue"> Father id: {{$id}}</font></h6>
-                <h4><font color="red">Dont forget the father id you will need it all over your life!!!</font> </h4>
-                <a href="{{route('motherSignUp')}}"><button id="button-style,button,button:hover">Continue to mother signup</button></a>
+        <div class="u-container-layout u-container-layout-1">
+            <h6>Father succesfully added to database.</h6>
+            <h6>Father name: {{$name}}</h6>
+            <h6><font color="blue"> Father id: {{$id}}</font></h6>
+            <h4><font color="red">Dont forget the father id you will need it all over your life!!!</font></h4>
+            <a href="{{route('motherSignUp')}}">
+                <button id="button-style,button,button:hover">Continue to mother signup</button>
+            </a>
 
-            </div>
         </div>
     </div>
 </section>
