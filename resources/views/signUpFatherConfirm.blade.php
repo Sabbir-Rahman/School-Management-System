32b516f2dc8c1009b75e5a8d16fb0da52d4ac2c4
SaiyaraOishi <56593485+SaiyaraOishi@users.noreply.github.com>
2021-01-02 20:20:32 +0600

    changes to signUpFatherConfirm.blade file

--- resources/views/signUpFatherConfirm.blade.php
+++ resources/views/signUpFatherConfirm.blade.php
@@ -1,6 +1,45 @@
-<h2>Father succesfully added to database</h2>
-<h2>Father name: {{$name}}</h2>
-<h2><font color="blue"> Father id: {{$id}}</font></h2>
-<h1><font color="red">Dont forget the father id you will need it all over your life !!!</font> </h1>
-<a href="{{route('motherSignUp')}}"><button>Continue to mother signup</button></a>
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Page 1</title>
+    <link rel="stylesheet" href="/css/signUpFatherConfirm.css" media="screen">
+    <link rel="stylesheet" href="/css/signUpFatherConfirm-1.css" media="screen">
+    <script class="u-script" type="text/javascript" src="/js/signUpFatherConfirm.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.2.3, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
 
+
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "index.html"
+}</script>
+    <meta property="og:title" content="Page 1">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+</head>
+<body data-home-page="Page-1.html" data-home-page-title="Page 1" class="u-body">
+<section class="u-clearfix u-image u-section-1" id="sec-e271" data-image-width="1280" data-image-height="960">
+    <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-container-style u-group u-opacity u-opacity-85 u-palette-1-base u-radius-50 u-shape-round u-group-1">
+            <div class="u-container-layout u-container-layout-1">
+                <h2>Father succesfully added to database</h2>
+                <h2>Father name: {{$name}}</h2>
+                <h2><font color="blue"> Father id: {{$id}}</font></h2>
+                <h1><font color="red">Dont forget the father id you will need it all over your life !!!</font> </h1>
+                <a href="{{route('motherSignUp')}}"><button id="button-style,button,button:hover">Continue to mother signup</button></a>
+
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
