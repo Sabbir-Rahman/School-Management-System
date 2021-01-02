06d0f3a856d7c8878bc1a57e57935452247d2060
SaiyaraOishi <56593485+SaiyaraOishi@users.noreply.github.com>
2021-01-02 20:57:26 +0600

    teacher's dashboard

---
+++ resources/views/teacherDashboard.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Attendance</title>
+    <link rel="stylesheet" href="teacherDashboard.css" media="screen">
+    <link rel="stylesheet" href="teacherDashboard-1.css" media="screen">
+    <script class="u-script" type="text/javascript" src="teacherDashboard.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.2.3, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+
+
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "Site1",
+		"url": "index.html"
+}</script>
+    <meta property="og:title" content="Attendance">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+</head>
+<body class="u-body u-overlap u-overlap-transparent"><header class="u-align-left u-clearfix u-gradient u-header u-header" id="sec-736b"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="https://nicepage.com/templates" class="u-active-none u-border-2 u-border-white u-btn u-btn-rectangle u-button-style u-hover-none u-none u-text-body-alt-color u-text-hover-grey-10 u-btn-1">Logout</a>
+        <div alt="" class="u-image u-image-circle u-image-1" data-image-width="582" data-image-height="830"></div>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+            <div class="menu-collapse">
+                <a class="u-button-style u-nav-link u-text-white" href="#">
+                    <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+                            </symbol>
+                        </defs></svg>
+                </a>
+            </div>
+            <div class="u-custom-menu u-nav-container">
+                <ul class="u-nav u-unstyled"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-white" href="Announcement.html">Announcement</a>
+                    </li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-white" href="Attendance.html">Attendance</a>
+                    </li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-white">Result</a>
+                    </li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-white">Routine</a>
+                    </li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-white">Subjects</a>
+                    </li></ul>
+            </div>
+            <div class="u-custom-menu u-nav-container-collapse">
+                <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+                    <div class="u-sidenav-overflow">
+                        <div class="u-menu-close"></div>
+                        <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Announcement.html">Announcement</a>
+                            </li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Attendance.html">Attendance</a>
+                            </li><li class="u-nav-item"><a class="u-button-style u-nav-link">Result</a>
+                            </li><li class="u-nav-item"><a class="u-button-style u-nav-link">Routine</a>
+                            </li><li class="u-nav-item"><a class="u-button-style u-nav-link">Subjects</a>
+                            </li></ul>
+                    </div>
+                </div>
+                <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+            </div>
+        </nav>
+    </div></header>
+<section class="u-align-center u-clearfix u-image u-section-1" id="carousel_e567" data-image-width="1280" data-image-height="854">
+    <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-list u-repeater u-list-1">
+            <div class="u-align-left u-container-style u-gradient u-list-item u-radius-50 u-repeater-item u-shape-round u-list-item-1">
+                <div class="u-container-layout u-similar-container u-container-layout-1">
+                    <h5 class="u-text u-text-body-alt-color u-text-1">Science</h5>
+                    <p class="u-text u-text-body-alt-color u-text-2">Class : 4<br>Section : Tulip
+                    </p>
+                </div>
+            </div>
+            <div class="u-align-left u-container-style u-gradient u-list-item u-radius-50 u-repeater-item u-shape-round u-list-item-2">
+                <div class="u-container-layout u-similar-container u-container-layout-2">
+                    <h5 class="u-text u-text-body-alt-color u-text-3">Music</h5>
+                    <p class="u-text u-text-body-alt-color u-text-4">Class : 2<br>Section : Nile
+                    </p>
+                </div>
+            </div>
+            <div class="u-align-left u-container-style u-gradient u-list-item u-radius-50 u-repeater-item u-shape-round u-list-item-3" data-href="https://nicepage.com">
+                <div class="u-container-layout u-similar-container u-container-layout-3">
+                    <h5 class="u-text u-text-body-alt-color u-text-5">English</h5>
+                    <p class="u-text u-text-body-alt-color u-text-6">Class : Nursery<br>Section : Amazon
+                    </p>
+                </div>
+            </div>
+        </div>
+        <img src="img/Music_line_illustration.png" alt="" class="u-image u-image-round u-opacity u-opacity-90 u-radius-50 u-image-1" data-image-width="800" data-image-height="600">
+        <img src="img/english.jpg" alt="" class="u-image u-image-round u-preserve-proportions u-radius-50 u-image-2" data-image-width="600" data-image-height="600">
+        <img src="img/Science2.png" alt="" class="u-image u-image-round u-opacity u-opacity-95 u-radius-50 u-image-3" data-image-width="1160" data-image-height="870">
+    </div>
+</section>
+</body>
+</html>
