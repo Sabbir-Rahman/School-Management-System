d751b4876f8fc82d29f9d0870f7a7fe0e4fa7e5c
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-22 14:17:40 +0600

    teacher table data retrieved

--- app/Http/Controllers/adminTeacherInfo.php
+++ app/Http/Controllers/adminTeacherInfo.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\teacher_info;
 
 class adminTeacherInfo extends Controller
 {
     //
     function index()
     {
-        return view('admin/adminTeacherInfo');
+        $data = teacher_info::all();
+        return view('admin/adminTeacherInfo',['teachers'=>$data]);
     }
 }
