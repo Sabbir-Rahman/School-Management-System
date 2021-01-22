dcc30ebf21d4435b57c7bcc93f77b233b5a3cfc1
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-22 20:28:53 +0600

    room table created with foreign key

--- database/migrations/2021_01_22_134904_create_rooms_table.php
+++ database/migrations/2021_01_22_134904_create_rooms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBuildingsTable extends Migration
+class CreateRoomsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,17 @@ class CreateBuildingsTable extends Migration
      */
     public function up()
     {
-        Schema::create('buildings', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->id();
-            $table->integer('branch');
-            $table->string('name');
-            $table->text('totalFloor');
-            $table->text('totalRoom');
-            $table->text('comment');
+            $table->integer('roomNo');
+            $table->integer('studentCapacity');
+            $table->unsignedBigInteger('buildingId');
+            $table->text('buildingName');
             $table->timestamps();
+            $table->foreign('buildingId')->references('id')->on('buildings');
         });
+
+
     }
 
     /**
@@ -31,6 +33,6 @@ class CreateBuildingsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('buildings');
+        Schema::dropIfExists('rooms');
     }
 }
