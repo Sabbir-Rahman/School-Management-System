bb1561deeb5e5f0312f413f0468386382fe3807a
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-22 22:49:22 +0600

    room added working on

--- app/Http/Controllers/adminRoomInfo.php
+++ app/Http/Controllers/adminRoomInfo.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\room;
+use App\Models\buildings;
 class adminRoomInfo extends Controller
 {
-    //
+    private $branchNo;
 
     function index(){
         return view('admin/adminRoomInfo');
+    }
+
+    function addDataView(Request $request){
+
+        $this->branchNo = $request->input('branchNo');
+
+        $data = buildings::where('branch',$this->branchNo)->get();
+
+        return view('admin/adminAddRooms');
+    }
+
+    function addData(Request $request){
+
+        $room = new room();
+        $room->roomNo = $request->input('roomNo');
+        $room->roomNo = $request->input('roomNo');
+
+        return $request->input();
     }
 }
