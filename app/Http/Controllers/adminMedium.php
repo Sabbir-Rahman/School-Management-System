b078ed6f1d4ab454c01af084af1ce303a083c194
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-23 01:58:54 +0600

    medium, group added and viwed from admin

---
+++ app/Http/Controllers/adminMedium.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\mediumTable;
+
+class adminMedium extends Controller
+{
+    //
+    function index()
+    {
+        $data = mediumTable::all();
+        return view('admin/adminMedium',['mediums'=>$data]);
+    }
+
+    function addData(Request $request){
+
+        $medium = new mediumTable();
+        $medium->name = $request->input('name');
+
+        $query = $medium->save();
+
+
+
+        if($query)
+            return redirect('/admin/Medium')->with('success','Data Saved');
+        else
+            return "Data not insert";
+    }
+}
