a8df4e686e41c3101d0f653aa1954d2180e65b6d
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-22 15:27:55 +0600

    building table added

---
+++ database/migrations/2021_01_22_082521_create_buildings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuildingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('branch');
+            $table->text('name');
+            $table->text('totalFloor');
+            $table->text('totalRoom');
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buildings');
+    }
+}
--- resources/views/admin/adminBuildingInfo.blade.php
+++ resources/views/admin/adminBuildingInfo.blade.php
@@ -1,1 +1,147 @@
-<h2>Welcome from admin buildings info</h2>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Admin Class</title>
+    <link rel="stylesheet" href="/css/adminClass.css" media="screen">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+<body>
+
+{{--add data modal--}}
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <form action="addBuilding" method="POST">
+                @csrf
+                <div class="modal-body">
+
+                    <div class="mb-3">
+                        <label>School Branch</label>
+                        <input type="number" name="branch" class="form-control" placeholder="Enter school branch">
+
+                    </div>
+                    <div class="mb-3">
+                        <label>Name</label>
+                        <input type="text" name="buildingName" class="form-control" placeholder="Enter building name">
+
+                    </div>
+                    <div class="mb-3">
+                        <label>Total Floor</label>
+                        <input type="number" name="totalFloor" class="form-control" placeholder="Total Floor Number">
+
+                    </div>
+                    <div class="mb-3">
+                        <label>Total Room</label>
+                        <input type="number" name="totalRoom" class="form-control" placeholder="Total Room Number">
+
+                    </div>
+                    <div class="mb-3">
+                        <label>Comment</label>
+                        <input type="text" name="comment" class="form-control" placeholder="Anything want to add">
+
+                    </div>
+
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit"  class="btn btn-primary">Save data</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+{{--end add data modal--}}
+
+
+<div id="particles-js">
+
+</div>
+<div id="container">
+    <h3><font color="white">Building Table</font></h3>
+    @if(count($errors)>0)
+
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    @if(\Session::has('success'))
+        <div class="alert alert-success">
+            <p>{{ \Session::get('success') }}</p>
+        </div>
+@endif
+
+<!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Add data with modal
+    </button>
+
+    <br><br>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand">Building Table</a>
+            <form action="" method="POST" class="d-flex">
+                @csrf
+                <input class="form-control me-2" type="search" name="search_data" placeholder="Search"
+                       aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    <table id="datatable" class="table table-dark table-striped">
+        <thead>
+        <th scope="col">Id</th>
+        <th scope="col">School Branch</th>
+        <th scope="col">Name</th>
+        <th scope="col">Total Floor</th>
+        <th scope="col">Total Room</th>
+        <th scope="col">Comment</th>
+        <th scope="col">Created</th>
+        <th scope="col">Updated</th>
+        <th scope="col">Action</th>
+        </thead>
+        <tbody>
+        @foreach($buildings as $building)
+            <tr>
+                <td scope="row">{{$building['id']}}</td>
+                <td scope="row">{{$building['branch']}}</td>
+                <td scope="row">{{$building['name']}}</td>
+                <td scope="row">{{$building['totalFloor']}}</td>
+                <td scope="row">{{$building['totalRoom']}}</td>
+                <td scope="row">{{$building['comment']}}</td>
+                <td scope="row">{{$building['created_at']}}</td>
+                <td scope="row">{{$building['updated_at']}}</td>
+                <td scope="row">
+                    <a href="" class="btn btn-primary">EDIT</a>
+                    <a href="" class="btn btn-danger">DELETE</a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
+
+
+<script type="text/javascript" src="/js/particles.js"></script>
+<script type="text/javascript" src="/js/app.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"
+        integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js"
+        integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj"
+        crossorigin="anonymous"></script>
+</body>
+</html>
