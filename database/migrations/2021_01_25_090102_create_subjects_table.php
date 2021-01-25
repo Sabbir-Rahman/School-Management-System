7a37b4a5e5b4840f8f4288de963063f38e5b45bf
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-25 15:06:16 +0600

    course and subject table created

--- database/migrations/2021_01_25_090102_create_subjects_table.php
+++ database/migrations/2021_01_25_090102_create_subjects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCoursesTable extends Migration
+class CreateSubjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateCoursesTable extends Migration
      */
     public function up()
     {
-        Schema::create('courses', function (Blueprint $table) {
-            $table->string('id')->unique();
-            $table->text('class');
-            $table->text('subject');
-            $table->text('medium');
-            $table->text('group');
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->text('subjectName')->unique();
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateCoursesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('courses');
+        Schema::dropIfExists('subjects');
     }
 }
