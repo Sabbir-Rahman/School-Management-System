cfd5aa5e90a2acafcb71aba337cd0122ce02bd85
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-25 23:47:26 +0600

    course table data saved

--- app/Http/Controllers/adminCourse.php
+++ app/Http/Controllers/adminCourse.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\classTable;
+use App\Models\course;
+use App\Models\group;
+use App\Models\mediumTable;
+use App\Models\subject;
 use Illuminate\Http\Request;
 
 class adminCourse extends Controller
 {
     //
     function index(){
-        return view('admin/adminCourse');
+
+        $data1 = classTable::all();
+        $data2 = mediumTable::all();
+        $data3 = group::all();
+        $data4 = subject::all();
+
+        return view('admin/adminCourse',['classes'=>$data1,'mediums'=>$data2,'groups'=>$data3,'subjects'=>$data4]);
+    }
+
+    function addData(Request $request){
+
+
+
+        $course = new course();
+
+        $course->class = $request->input('search_option_class');
+        $course->subject = $request->input('search_option_subject');
+        $course->paper = $request->input('search_option_paper');
+        $course->medium = $request->input('search_option_medium');
+        $course->group = $request->input('search_option_group');
+
+        $query = $course->save();
+
+
+        if($query)
+            return redirect('/admin/course')->with('success','Data Saved');
+        else
+            return "Data not insert";
+
     }
 }
