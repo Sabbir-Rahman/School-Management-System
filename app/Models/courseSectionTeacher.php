f9468ec1ecb575856446e3f428db713cb57d9fa1
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-27 11:33:48 +0600

    course section teacher table created

---
+++ app/Models/courseSectionTeacher.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class courseSectionTeacher extends Model
+{
+    use HasFactory;
+}
---
+++ resources/views/admin/courseSectionTeacherAddSection.blade.php
@@ -0,0 +1,1 @@
+<h2>Welcome to add section</h2>
