02a732eac45ec10517e4c75eb664107ff441e1d7
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-02-01 01:11:15 +0600

    home work page teacher ok

--- resources/views/teacher/teacherDashboard.blade.php
+++ resources/views/teacher/teacherDashboard.blade.php
@@ -55,7 +55,7 @@
                     </a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link" href="./dashboard.html">
+                    <a class="nav-link" href="{{route('teacher.goToHomeWorkPage')}}">
 
                         <p>Home Work</p>
                     </a>
