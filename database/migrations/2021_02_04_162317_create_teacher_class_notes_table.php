58220b40ec33f72a7de11eb3d67051fe44f9361e
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-02-04 22:26:04 +0600

    class notes table created

---
+++ database/migrations/2021_02_04_162317_create_teacher_class_notes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeacherClassNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teacher_class_notes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('courseSectionTeacherId');
+            $table->string('sectionId');
+            $table->string('course');
+            $table->string('paper');
+            $table->string('teacherName');
+            $table->text('classNotesTitle');
+            $table->text('classNotesDetails');
+            $table->date('date');
+            $table->text('file')->nullable();
+            $table->text('comment')->nullable();
+            $table->foreign('courseSectionTeacherId')->references('id')->on('course_section_teachers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teacher_class_notes');
+    }
+}
