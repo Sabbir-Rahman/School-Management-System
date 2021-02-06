71d9d3a5b86a4bf7f45ec8746163d53dab7f4534
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-02-07 05:30:48 +0600

    Student Financial View Created

--- app/Http/Controllers/adminStudentFinancial.php
+++ app/Http/Controllers/adminStudentFinancial.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\view_adminStudentFinancial;
 
 class adminStudentFinancial extends Controller
 {
     function goToStudentFinancialCondition(){
 
-        return view('admin/adminStudentFinancialStatusView');
+            $student = view_adminStudentFinancial::select("*")->get();
+
+
+        return view('admin/adminStudentFinancialStatusView',['students'=>$student]);
     }
 
     function goToStudentPayment(){
